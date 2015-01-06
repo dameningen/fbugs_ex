b904b9029555bce66bb39c12ce90679ef869945c
toumat <a@foo.jp>
2015-01-06 12:39:42 +0900

    test

--- src/test/java/sample/DetectorDriver.java
+++ src/test/java/sample/DetectorDriver.java
@@ -31,146 +31,146 @@ import edu.umd.cs.findbugs.classfile.MethodDescriptor;
 import edu.umd.cs.findbugs.classfile.impl.ClassFactory;
 
 /**
- *
+ * a
  */
 public class DetectorDriver {
 
-    private static final String WORK_SPACE_PATH = "D:/eclipse/workspace";
+	private static final String WORK_SPACE_PATH = "D:/eclipse/workspace";
 
-    public static void main(String[] args) throws Exception {
-        BugReporter bugReporter = new PrintStreamBugReporter(System.out);
-        Detector detector = new DetectorTutorial(bugReporter);
-        Detector detector2 = new LengthCompareDetector(bugReporter);
-        BugPattern bugPattern = new BugPattern("TUTORIAL_BUG", "TU",
-                "CORRECTNESS", false, "短い説明", "長い説明", "詳細");
-        ProjectStats stats = analyze(WORK_SPACE_PATH
-                + "/test/bin/test/Target.class", detector2, bugPattern,
-                bugReporter);
-        System.out.println("バグ数[" + stats.getTotalBugs() + "]");
-        System.out.println(stats.toString());
-        stats.reportSummary(System.out);
-    }
+	public static void main(String[] args) throws Exception {
+		BugReporter bugReporter = new PrintStreamBugReporter(System.out);
+		Detector detector = new DetectorTutorial(bugReporter);
+		Detector detector2 = new LengthCompareDetector(bugReporter);
+		BugPattern bugPattern = new BugPattern("TUTORIAL_BUG", "TU",
+				"CORRECTNESS", false, "短い説明", "長い説明", "詳細");
+		ProjectStats stats = analyze(WORK_SPACE_PATH
+				+ "/test/bin/test/Target.class", detector2, bugPattern,
+				bugReporter);
+		System.out.println("バグ数[" + stats.getTotalBugs() + "]");
+		System.out.println(stats.toString());
+		stats.reportSummary(System.out);
+	}
 
-    private static ProjectStats analyze(String filePath, Detector detector,
-            BugPattern bugPattern, BugReporter bugReporter)
-            throws CheckedAnalysisException, IOException, InterruptedException {
+	private static ProjectStats analyze(String filePath, Detector detector,
+			BugPattern bugPattern, BugReporter bugReporter)
+			throws CheckedAnalysisException, IOException, InterruptedException {
 
-        // internal to FindBugs, the code uses the Detector2 interface
-        Detector2 det = new DetectorToDetector2Adapter(detector);
+		// internal to FindBugs, the code uses the Detector2 interface
+		Detector2 det = new DetectorToDetector2Adapter(detector);
 
-        // register the rules message
-        // I18N.instance().registerBugPattern(bugPattern);
-        // FindBugs2.0.1ではこうらしい
-        DetectorFactoryCollection.instance().registerBugPattern(bugPattern);
+		// register the rules message
+		// I18N.instance().registerBugPattern(bugPattern);
+		// FindBugs2.0.1ではこうらしい
+		DetectorFactoryCollection.instance().registerBugPattern(bugPattern);
 
-        // a great deal of code to say
-        // 'analyze the files in this directory'
-        IClassFactory classFactory = ClassFactory.instance();
-        IClassPath classPath = classFactory.createClassPath();
-        IAnalysisCache analysisCache = classFactory.createAnalysisCache(
-                classPath, bugReporter);
-        Global.setAnalysisCacheForCurrentThread(analysisCache);
-        FindBugs2.registerBuiltInAnalysisEngines(analysisCache);
-        IClassPathBuilder builder = classFactory
-                .createClassPathBuilder(bugReporter);
-        ICodeBaseLocator locator = classFactory
-                .createFilesystemCodeBaseLocator(filePath);
-        builder.addCodeBase(locator, true);
-        builder.build(classPath, new NoOpFindBugsProgress());
-        List<ClassDescriptor> classesToAnalyze = builder.getAppClassList();
-        AnalysisCacheToAnalysisContextAdapter analysisContext = new AnalysisCacheToAnalysisContextAdapter();
-        AnalysisContext.setCurrentAnalysisContext(analysisContext);
+		// a great deal of code to say
+		// 'analyze the files in this directory'
+		IClassFactory classFactory = ClassFactory.instance();
+		IClassPath classPath = classFactory.createClassPath();
+		IAnalysisCache analysisCache = classFactory.createAnalysisCache(
+				classPath, bugReporter);
+		Global.setAnalysisCacheForCurrentThread(analysisCache);
+		FindBugs2.registerBuiltInAnalysisEngines(analysisCache);
+		IClassPathBuilder builder = classFactory
+				.createClassPathBuilder(bugReporter);
+		ICodeBaseLocator locator = classFactory
+				.createFilesystemCodeBaseLocator(filePath);
+		builder.addCodeBase(locator, true);
+		builder.build(classPath, new NoOpFindBugsProgress());
+		List<ClassDescriptor> classesToAnalyze = builder.getAppClassList();
+		AnalysisCacheToAnalysisContextAdapter analysisContext = new AnalysisCacheToAnalysisContextAdapter();
+		AnalysisContext.setCurrentAnalysisContext(analysisContext);
 
-        // finally, perform the analysis
-        for (ClassDescriptor d : classesToAnalyze) {
-            det.visitClass(d);
-        }
+		// finally, perform the analysis
+		for (ClassDescriptor d : classesToAnalyze) {
+			det.visitClass(d);
+		}
 
-        return bugReporter.getProjectStats();
-    }
+		return bugReporter.getProjectStats();
+	}
 
-    private static class PrintStreamBugReporter implements BugReporter {
-        private ProjectStats stats = new ProjectStats();
-        private PrintStream os;
-        private List<BugReporterObserver> observers = new ArrayList<BugReporterObserver>();
+	private static class PrintStreamBugReporter implements BugReporter {
+		private ProjectStats stats = new ProjectStats();
+		private PrintStream os;
+		private List<BugReporterObserver> observers = new ArrayList<BugReporterObserver>();
 
-        public PrintStreamBugReporter(PrintStream os) {
-            this.os = os;
-        }
+		public PrintStreamBugReporter(PrintStream os) {
+			this.os = os;
+		}
 
-        @Override
-        public void addObserver(BugReporterObserver arg0) {
-            observers.add(arg0);
-        }
+		@Override
+		public void addObserver(BugReporterObserver arg0) {
+			observers.add(arg0);
+		}
 
-        @Override
-        public void finish() {
-        }
+		@Override
+		public void finish() {
+		}
 
-        @Override
-        public ProjectStats getProjectStats() {
-            return stats;
-        }
+		@Override
+		public ProjectStats getProjectStats() {
+			return stats;
+		}
 
-        public BugReporter getRealBugReporter() {
-            return this;
-        }
+		public BugReporter getRealBugReporter() {
+			return this;
+		}
 
-        @Override
-        public void reportBug(BugInstance arg0) {
-            stats.addBug(arg0);
-            for (BugReporterObserver observer : observers) {
-                observer.reportBug(arg0);
-            }
-            os.println(arg0.getAbridgedMessage());
-        }
+		@Override
+		public void reportBug(BugInstance arg0) {
+			stats.addBug(arg0);
+			for (BugReporterObserver observer : observers) {
+				observer.reportBug(arg0);
+			}
+			os.println(arg0.getAbridgedMessage());
+		}
 
-        @Override
-        public void reportQueuedErrors() {
-        }
+		@Override
+		public void reportQueuedErrors() {
+		}
 
-        @Override
-        public void setErrorVerbosity(int arg0) {
-        }
+		@Override
+		public void setErrorVerbosity(int arg0) {
+		}
 
-        @Override
-        public void setPriorityThreshold(int arg0) {
-        }
+		@Override
+		public void setPriorityThreshold(int arg0) {
+		}
 
-        @Override
-        public void logError(String arg0) {
-            // os.println(arg0.getBytes());
-        }
+		@Override
+		public void logError(String arg0) {
+			// os.println(arg0.getBytes());
+		}
 
-        @Override
-        public void logError(String arg0, Throwable arg1) {
-            // os.println(arg0.getBytes());
-            // arg1.printStackTrace(os);
-        }
+		@Override
+		public void logError(String arg0, Throwable arg1) {
+			// os.println(arg0.getBytes());
+			// arg1.printStackTrace(os);
+		}
 
-        @Override
-        public void reportMissingClass(ClassNotFoundException arg0) {
-            arg0.printStackTrace(os);
-        }
+		@Override
+		public void reportMissingClass(ClassNotFoundException arg0) {
+			arg0.printStackTrace(os);
+		}
 
-        @Override
-        public void reportMissingClass(ClassDescriptor arg0) {
-            os.println("Class not found: " + arg0);
-        }
+		@Override
+		public void reportMissingClass(ClassDescriptor arg0) {
+			os.println("Class not found: " + arg0);
+		}
 
-        @Override
-        public void reportSkippedAnalysis(MethodDescriptor arg0) {
-            os.println("Skipped Method: " + arg0);
-        }
+		@Override
+		public void reportSkippedAnalysis(MethodDescriptor arg0) {
+			os.println("Skipped Method: " + arg0);
+		}
 
-        @Override
-        public void observeClass(ClassDescriptor arg0) {
-        }
+		@Override
+		public void observeClass(ClassDescriptor arg0) {
+		}
 
-        @Override
-        public BugCollection getBugCollection() {
-            return null;
-        }
+		@Override
+		public BugCollection getBugCollection() {
+			return null;
+		}
 
-    }
+	}
 }
